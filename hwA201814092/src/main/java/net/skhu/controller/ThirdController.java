2c93b84efcbb2e5f6bf42d03d572e89071b252c1
jiyeong <jiyeongstar@gmail.com>
2021-09-18 23:23:19 +0900

    spring기초-파라미터전달

--- hwA201814092/src/main/java/net/skhu/controller/ThirdController.java
+++ hwA201814092/src/main/java/net/skhu/controller/ThirdController.java
@@ -3,6 +3,7 @@ package net.skhu.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.skhu.dto.Student;
 
@@ -16,6 +17,19 @@ public class ThirdController {
 		return "third/test1";
 	}
 	
+    @RequestMapping(value="test3", method=RequestMethod.GET)
+    public String test3(Model model) {
+        model.addAttribute("student", new Student());
+        return "third/test3";
+    }
+
+    @RequestMapping(value="test3", method=RequestMethod.POST)
+    public String test3(Model model, Student student) {
+        model.addAttribute("message", "저장되었습니다");
+        return "third/test3";
+    }
+
+
 }
 
 
